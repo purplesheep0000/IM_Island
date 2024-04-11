95b516bc72d2bd523cc38115460ec6077f0472f8
purplesheep0000 <a44281234@gmail.com>
2024-04-11 17:06:21 +0800

    f

--- Backend/main.py
+++ Backend/main.py
@@ -8,7 +8,8 @@ from datetime import datetime
 app = FastAPI()
 
 # 创建引擎和会话
-engine = create_engine("postgresql://postgres:20030629@localhost/IM_Island")
+engine = create_engine("postgresql://postgres:DpIXwQjiOwpcLlpgyofHZZvhzlAjbMRD@monorail.proxy.rlwy.net:22660/railway")
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # 创建基类
@@ -16,38 +17,26 @@ Base = declarative_base()
 
 # 用户模型
 class User(Base):
-    __tablename__ = "users"
-
-    user_id = Column(Integer, primary_key=True, index=True)
-    resul = Column(String(50), nullable=False)
+    __tablename__ = "users_2"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)  # 添加 autoincrement=True
     result_time = Column(TIMESTAMP, server_default="NOW()")
 
 # 创建表
 Base.metadata.create_all(bind=engine)
 
-# 创建新用户时分配用户ID
-def create_user(db: Session, resul: str):
-    new_user = User(resul=resul)
+# 定义获取数据库会话的函数
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# 定义路由
+@app.post("/time")
+async def time(db: Session = Depends(get_db)):
+    new_user = User(result_time=datetime.now())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
-
-# 路由示例
-@app.post("/users/")
-async def create_user_api(resul: str, db: Session = Depends(SessionLocal)):
-    return create_user(db, resul)
-
-@app.post("/click")
-async def click(user_id: int, db: Session = Depends(SessionLocal)):
-    current_time = datetime.now()
-    db.execute(users.insert().values(user_id=user_id, result_time=current_time))
-    db.commit()
-    return {"message": "Click recorded successfully!"}
-
-@app.post("/answer")
-async def answer(user_id: int, opt: str, page: str, db: Session = Depends(SessionLocal)):
-    ans = 'green_tea'
-    db.execute(users.insert().values(user_id=user_id, resul=ans))
-    db.commit()
-    return {"message": "Answer recorded successfully!"}
