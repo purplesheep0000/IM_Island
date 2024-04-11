594fe926f6688675468120118ea410f6d8c37951
purplesheep0000 <a44281234@gmail.com>
2024-04-11 21:00:16 +0800

    4000

--- Backend/server.js
+++ Backend/server.js
@@ -42,6 +42,8 @@ app.post('/time', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000; // Use the provided port or default to 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
