27c59b3b238b151b713ff25cb4a239afdedb8bc7
purplesheep0000 <a44281234@gmail.com>
2024-04-13 12:17:43 +0800

    backend :q :q

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const { Sequelize, DataTypes } = require('sequelize');
 const { Op } = require('sequelize');
-const cors = require('cors'); // 引入 cors 中间件
 
 const app = express();
-app.use(cors()); // 使用 cors 中间件
 
 // 创建 Sequelize 实例
 const sequelize = new Sequelize('railway', 'postgres', 'DpIXwQjiOwpcLlpgyofHZZvhzlAjbMRD', {
